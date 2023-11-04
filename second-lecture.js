16026d46eb4a8e87d7c12e62cb877bc65fe6d91f
Esma Ekmekci <esoes@Esmas-MacBook-Air.local>
2023-11-04 16:50:38 +0300

    routing based on url and getting message from user

--- second-lecture.js
+++ second-lecture.js
@@ -25,9 +25,46 @@
 // server.listen(3000);
 
 //3- calling specific part of request
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+// const server = http.createServer((req, res) => {
+//     console.log(req.url, req.method, req.headers);
+// });
+
+// server.listen(3000);
+
+//4 - sending response
+// const http = require('node:http');
+
+// const server = http.createServer(async (req, res) =>{
+//     res.setHeader('Content-Type', 'text/html');
+//     res.write('<html>');
+//     res.write('<head><title>First Page</title></head><body><h1>Hello!</h1></body>');
+//     res.write('</html>');
+//     res.end();
+    
+// });
+// server.listen(3000);
+
+
+//5 -routing based on url and getting message from user
+const  http = require('http');
+const server = http.createServer((req, res) =>{
+    const url = req.url;
+    //if there is only / 
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>First Page</title></head>');
+        res.write('<body><form action="/message" method="POST" ><input type="text"><button type="submit">Send</button></form></body>')
+        res.write('</html>');
+        return res.end();
+    }
+
+    res.write('<html>');
+    res.write('<head><title>First Page</title></head><body><h1>Hello!</h1></body>');
+    res.write('</html>');
+    res.end();
+
+    
 });
-
+server.listen(3000);
